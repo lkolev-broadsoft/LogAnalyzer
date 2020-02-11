d1d8379f8235f4666419a43e6825affbef888862
Lyubomir Kolev <lkolev@broadsoft.com>
2020-02-11 14:14:30 +0200

    Working with tar.gz input files using Apache commons compress and Java NIO(There is a problem in compress)

--- src/main/java/com/cisco/LogAnalyzer.java
+++ src/main/java/com/cisco/LogAnalyzer.java
@@ -1,18 +1,22 @@
 package com.cisco;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.zip.GZIPInputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
+import org.apache.commons.compress.archivers.ArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
-
 
 
 public class LogAnalyzer {
@@ -20,8 +24,6 @@ public class LogAnalyzer {
     protected InputStream inputStream;
 
     protected String inputArchiveFilePath;
-
-    protected TarArchiveInputStream  tarInputStream;
 
     protected IMPLogAnalyzer impLogAnalyzer = new IMPLogAnalyzer();
 
@@ -31,8 +33,11 @@ public class LogAnalyzer {
 
     protected Enumeration<? extends ZipEntry> entries;
 
+    protected Path archivePath;
+
     public LogAnalyzer(String inputArchiveFilePath){
         this.inputArchiveFilePath = inputArchiveFilePath;
+        this.archivePath = Paths.get(inputArchiveFilePath);
         determineArchiveType(inputArchiveFilePath);
     }
 
@@ -46,11 +51,17 @@ public class LogAnalyzer {
         if(m.find()){
             if(m.group().equals(m.group(1))){
                 openZIPFile();
-                openZIPFileReadStats();
+//                openZIPFileReadStats();
             }
             else if(m.group().equals(m.group(2))){
-               openTarGZFile();
-//               openTarGZFileReadStats();
+               //openTarGZFile();
+//               //openTarGZFileReadStats();
+                try {
+                    unTarGZ(archivePath);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
             }
             else {
                 System.out.println("Unsupported input type, only zip and tar.gz files are accepted.");
@@ -111,23 +122,70 @@ public class LogAnalyzer {
         }
     }
 
-    protected void openTarGZFile() {
-        try (TarArchiveInputStream tarInput = new TarArchiveInputStream(new GzipCompressorInputStream(new FileInputStream(inputArchiveFilePath)))) {
-            int filecount = 0;
-            TarArchiveEntry currentEntry = tarInput.getNextTarEntry();
-            while (currentEntry != null) {
-                if (!currentEntry.isDirectory() && currentEntry.getName().contains("IMPLog")) {
-                    listOfFiles.add(currentEntry.getName());
-                    impLogAnalyzer.writeToOutputTxtFile(("result" + listOfFiles.get(filecount)), impLogAnalyzer.analyzeLog(tarInput));
-                    filecount++;
-                    currentEntry = tarInput.getNextTarEntry(); // iterate to the next file
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//    // Problem with TarArchiveNextEntry - open the file and go though the entries one by one(send the input stream first)
+//
+//    protected TarArchiveInputStream getTarAchiveInputStream(String inputTarFilePath) throws FileNotFoundException {
+//        return new TarArchiveInputStream(new FileInputStream(inputTarFilePath));
+//    }
+//
+////    protected FileInputStream getInputFileStream(String inputFilePath){
+////        return new FileInputStream(inputFilePath);
+////    }
 
+    //Try with using both apache commons and Java NIO(Path from String instead of file)
+
+    protected void unTarGZ(Path pathInput) throws IOException {
+        TarArchiveInputStream tarArchiveInputStream = new TarArchiveInputStream(new GzipCompressorInputStream(new BufferedInputStream(Files.newInputStream(pathInput))));
+        ArchiveEntry currentEntry = null;
+        int filecount = 0;
+        while ((currentEntry = tarArchiveInputStream.getNextEntry()) != null) {
+            if (!currentEntry.isDirectory() && currentEntry.getName().contains("IMPLog")) {
+                listOfFiles.add(currentEntry.getName());
+                impLogAnalyzer.writeToOutputTxtFile(("result" + listOfFiles.get(filecount)), impLogAnalyzer.analyzeLog(tarArchiveInputStream));
+                filecount++;
+            }
+            tarArchiveInputStream.close();
+        }
     }
+
+//        protected void openTarGZFile() {
+//        try (FileInputStream fileInputStream = new FileInputStream(inputArchiveFilePath);
+//             GZIPInputStream gzipInputStream = new GZIPInputStream(fileInputStream);
+//             TarArchiveInputStream tarArchiveInputStream = new TarArchiveInputStream(gzipInputStream)){
+//            int filecount = 0;
+//            TarArchiveEntry currentEntry;
+//            while (((currentEntry = tarArchiveInputStream.getNextTarEntry()) != null)) {
+//                if (!currentEntry.isDirectory() && currentEntry.getName().contains("IMPLog")) {
+//                    listOfFiles.add(currentEntry.getName());
+//                    impLogAnalyzer.writeToOutputTxtFile(("result" + listOfFiles.get(filecount)), impLogAnalyzer.analyzeLog(tarArchiveInputStream));
+//                    filecount++;
+//                }
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+
+// java.io.IOException: Stream Closed ??
+//    protected void openTarGZFile() {
+//        try (TarArchiveInputStream tarInput = new TarArchiveInputStream(new GzipCompressorInputStream(new FileInputStream(inputArchiveFilePath)))) {
+//            int filecount = 0;
+//            TarArchiveEntry currentEntry = tarInput.getNextTarEntry();
+//            while (currentEntry != null) {
+//                if (!currentEntry.isDirectory() && currentEntry.getName().contains("IMPLog")) {
+//                    listOfFiles.add(currentEntry.getName());
+//                    System.out.println("In the while loop");
+//                    impLogAnalyzer.writeToOutputTxtFile(("result" + listOfFiles.get(filecount)), impLogAnalyzer.analyzeLog(tarInput));
+//                    filecount++;
+//                    currentEntry = tarInput.getNextEntry(); // iterate to the next file
+//                }
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
 
     //Something is wrong with reading from tar.gz => java.lang.NullPointerException
     //	at org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream.read(GzipCompressorInputStream.java:296)
