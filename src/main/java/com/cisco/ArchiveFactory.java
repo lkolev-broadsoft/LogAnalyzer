dd85af4747e0967a2f4dbda1abe090ff32b09f56
Lyubomir Kolev <lkolev@broadsoft.com>
2020-02-21 17:30:43 +0200

    Cleanup commented code

--- src/main/java/com/cisco/ArchiveFactory.java
+++ src/main/java/com/cisco/ArchiveFactory.java
@@ -10,26 +10,6 @@ public interface ArchiveFactory {
 
     void open(String inputArchiveFilePath);
 
-//    default String determineArchiveType(String inputArchiveFile){
-//        String pattern = "(\\.zip)|(\\.tar\\.gz)";
-//        String archiveType = "DIRECTORY";
-//        Pattern r = Pattern.compile(pattern);
-//        Matcher m = r.matcher(inputArchiveFile);
-//        if(m.find()){
-//            if(m.group().equals(m.group(1))){
-//                archiveType = "ZIP";
-//            }
-//            else if(m.group().equals(m.group(2))){
-//                archiveType = "TAR.GZ";
-//            }
-//            else {
-//                System.out.println("Unsupported input type, only zip and tar.gz files are accepted.");
-//            }
-//            archiveType = "DIRECTORY";
-//        }
-//        return archiveType;
-//    }
-
         static void determineArchiveType(String inputArchiveFilePath){
         String pattern = "(\\.zip)|(\\.tar\\.gz)";
         Pattern r = Pattern.compile(pattern);
--- src/main/java/com/cisco/LogAnalyzer.java
+++ src/main/java/com/cisco/LogAnalyzer.java
@@ -2,9 +2,6 @@ package com.cisco;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 
 public class LogAnalyzer {
 
@@ -12,35 +9,9 @@ public class LogAnalyzer {
 
     protected Path archivePath;
 
-//    protected ZIPedLogsFactory zipLogs = new ZIPedLogsFactory();
-//
-//    protected TARGZedLogsFactory tarGZLogs = new TARGZedLogsFactory();
-
     public LogAnalyzer(String inputArchiveFilePath){
         this.inputArchiveFilePath = inputArchiveFilePath;
         this.archivePath = Paths.get(inputArchiveFilePath);
-//        determineArchiveType(inputArchiveFilePath);
         ArchiveFactory.determineArchiveType(inputArchiveFilePath);
     }
-
-    //Take actions according to the type of logs provided.
-    //Go through all of the logs
-
-//    protected void determineArchiveType(String inputArchiveFile){ //Make public
-//        String pattern = "(\\.zip)|(\\.tar\\.gz)";
-//        Pattern r = Pattern.compile(pattern);
-//        Matcher m = r.matcher(inputArchiveFile);
-//        if(m.find()){
-//            if(m.group().equals(m.group(1))){
-//                zipLogs.open(inputArchiveFilePath);
-//            }
-//            else if(m.group().equals(m.group(2))){
-//                tarGZLogs.open(inputArchiveFilePath);
-//            }
-//            else {
-//                System.out.println("Unsupported input type, only zip and tar.gz files are accepted.");
-//            }
-//        }
-//    }
-
 }
